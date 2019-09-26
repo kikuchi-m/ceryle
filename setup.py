ed24e73bebd1b96af5716f53fe8d2a462a2ccf5f
Kikuchi Motoki <wildflower.pink0102@gmail.com>
2019-09-26 21:37:47 +0900

    Release v0.1.2

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ import setuptools
 setuptools.setup(
     name='ceryle',
     description='Task based command runner tool',
-    version='0.1.1',
+    version='0.1.2',
     python_requires='>=3.6.5',
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': ['ceryle = ceryle.main:entry_point'],
     },
     url='https://github.com/kikuchi-m/ceryle',
+    author='Kikuchi Motoki',
+    author_email='wildflower.pink0102@gmail.com',
     license='MIT',
     clissifiers=[
         'Development Status :: 3 - Alpha',
