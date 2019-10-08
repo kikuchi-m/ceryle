45ac4b30fd83f847f1bf0a9c6553164152e3af34
Kikuchi Motoki <wildflower.pink0102@gmail.com>
2019-10-08 15:18:19 +0900

    Load extensions separately with task files

--- ceryle/dsl/aggregate_loader.py
+++ ceryle/dsl/aggregate_loader.py
@@ -11,22 +11,23 @@ class AggregateTaskFileLoader:
     def load(self):
         tasks = {}
         default = None
-        gvars = {}
         lvars = {}
-        # TODO: load extensions and task files separately
+        for f in self._extensions:
+            x = ceryle.ExtensionLoader(f).load(
+                local_vars=lvars.copy(),
+                additional_args=self._additional_args)
+            lvars.update(x)
         for f in self._files:
             d = ceryle.TaskFileLoader(f).load(
-                global_vars=gvars,
-                local_vars=lvars,
+                local_vars=lvars.copy(),
                 additional_args=self._additional_args)
             for t in d.tasks:
                 if t.name in tasks:
                     util.print_out(f'warn: {t.name} is overwritten')
                 tasks.update({t.name: t})
-            default = d.default_task
-            gvars = d.global_vars
-            lvars = d.local_vars
-        return ceryle.dsl.loader.TaskDefinition(list(tasks.values()), default)
+            if d.default_task:
+                default = d.default_task
+        return ceryle.TaskDefinition(list(tasks.values()), default)
 
 
 def load_task_files(files, extensions, additional_args={}):
