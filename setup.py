01b718f6176f8ebdd588de88f7bfd0b34d9aeca4
Kikuchi Motoki <wildflower.pink0102@gmail.com>
2019-10-10 15:32:52 +0900

    Release v0.1.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='ceryle',
     description='Task based command runner tool',
-    version='0.1.7',
+    version='0.1.8',
     python_requires='>=3.6.5',
     packages=setuptools.find_packages(),
     entry_points={
