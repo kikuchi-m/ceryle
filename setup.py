d4a0d204b685545a53cd71d35661fc8a2b9b7c9f
Kikuchi Motoki <wildflower.pink0102@gmail.com>
2022-01-03 04:43:05 +0900

    Update supported python version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     name='ceryle',
     description='Task based command runner tool',
     version=read_version(),
-    python_requires='>=3.6.5',
+    python_requires='>=3.8.0',
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': ['ceryle = ceryle.main:entry_point'],
